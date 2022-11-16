df8225a336f51a37369f191292f458becd64169b
farisan <muhammad.farisan99@gmail.com>
2022-11-17 01:05:10 +0700

    add router home

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 
+// Import Pages
 import ForgotPassword from './pages/forgotpassword/ForgotPassword'
+import Home from './pages/home/Home'
+
 const router = createBrowserRouter([
+    { path: "/", element: <Home /> },
     { path: "/forgotpassword", element: <ForgotPassword /> },
 ]);
 
