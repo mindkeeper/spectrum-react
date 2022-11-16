93830b6eb32ef6b12e2f0c8aa15e257e3d26609f
farisan <muhammad.farisan99@gmail.com>
2022-11-16 16:20:00 +0700

    Footer

---
+++ src/component/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+function Footer() {
+    return (
+        <>
+            <h1>sss</h1>
+            <i class="fa-brands fa-facebook"></i>
+        </>
+    )
+}
+
+export default Footer
