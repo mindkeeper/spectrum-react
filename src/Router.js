167db9eb6471256af4c3e5d52d7351fcf373bb29
farisan <muhammad.farisan99@gmail.com>
2022-11-16 16:19:50 +0700

    komen

---
+++ src/Router.js
@@ -0,0 +1,9 @@
+// import { createBrowserRouter } from "react-router-dom";
+
+// import Footer from "./component/footer/Footer"
+// const router = createBrowserRouter([
+//     // { path: "/", element: <App />, errorElement: <Error /> },
+//     { path: "/", element: <Footer /> },
+// ]);
+
+// export default router;
