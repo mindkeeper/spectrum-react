d151f3e5c50c9f52bac19cb394addf250d07bbf0
rofa <zainrofa29@gmail.com>
2022-11-17 14:44:39 +0700

    add login route

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 
 // Import Pages
-import ForgotPassword from './pages/forgotpassword/ForgotPassword'
-import Home from './pages/home/Home'
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import ForgotPassword from "./pages/forgotpassword/ForgotPassword";
 
 const router = createBrowserRouter([
-    { path: "/", element: <Home /> },
-    { path: "/forgotpassword", element: <ForgotPassword /> },
+  { path: "/", element: <Home /> },
+  { path: "/my-account/login", element: <Login /> },
+  { path: "/forgotpassword", element: <ForgotPassword /> },
 ]);
 
 export default router;
