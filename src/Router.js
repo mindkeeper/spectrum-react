a63d5ffe96f9a865d708e83b4d28d1e640963928
farisan <muhammad.farisan99@gmail.com>
2022-11-17 17:56:50 +0700

    add router (profile seller, customer)

--- src/Router.js
+++ src/Router.js
@@ -3,9 +3,13 @@ import { createBrowserRouter } from "react-router-dom";
 // Import Pages
 import ForgotPassword from './pages/forgotpassword/ForgotPassword'
 import Home from './pages/home/Home'
+import ProfileCustomer from './pages/profile/profile customer/ProfileCustomer'
+import ProfileSeller from './pages/profile/profile seller/ProfileSeller'
 
 const router = createBrowserRouter([
     { path: "/", element: <Home /> },
+    { path: "/profile/customer", element: <ProfileCustomer /> },
+    { path: "/profile/seller", element: <ProfileSeller /> },
     { path: "/forgotpassword", element: <ForgotPassword /> },
 ]);
 
