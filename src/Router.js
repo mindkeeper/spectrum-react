70ffbb65a55a6e24d11901f651c6d8270ff3da3c
rofa <zainrofa29@gmail.com>
2022-11-17 15:20:03 +0700

    add regist route

--- src/Router.js
+++ src/Router.js
@@ -3,11 +3,13 @@ import { createBrowserRouter } from "react-router-dom";
 // Import Pages
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import Regist from "./pages/regist/Regist";
 import ForgotPassword from "./pages/forgotpassword/ForgotPassword";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/my-account/login", element: <Login /> },
+  { path: "/my-account/new", element: <Regist /> },
   { path: "/forgotpassword", element: <ForgotPassword /> },
 ]);
 
