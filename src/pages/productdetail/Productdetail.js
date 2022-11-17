c7825fb0485b826690665cb73c1a2b96390d09a2
farisan <muhammad.farisan99@gmail.com>
2022-11-18 00:59:00 +0700

    on going

---
+++ src/pages/productdetail/Productdetail.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// import css from "./Productdetail.module.css"
+
+function Productdetail() {
+    return (
+        <>
+            <div>Productdetail</div>
+        </>
+    )
+}
+
+export default Productdetail;
