82e07760db8e80247b286f6db32238437ab24517
rofa <zainrofa29@gmail.com>
2022-11-18 17:15:28 +0700

    add forget password page

--- src/pages/forgotpassword/ForgotPassword.js
+++ src/pages/forgotpassword/ForgotPassword.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import styles from "./ForgotPassword.module.css";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import authActions from "../../redux/actions/auths";
 
 function ForgotPassword() {
+  const navigate = useNavigate();
+  const dispacth = useDispatch();
+  const [body, setBody] = useState({});
+
+  const changeHandler = (e) =>
+    setBody({
+      ...body,
+      [e.target.name]: e.target.value,
+    });
+  console.log(body);
+
+  const to = () => navigate("/forget-password/new");
+
+  const submitHandler = () => {
+    dispacth(authActions.resetThunk(body, to));
+    localStorage.setItem("email", JSON.stringify(body.email));
+  };
+
   return (
     <>
       <Header />
@@ -22,7 +43,7 @@ function ForgotPassword() {
           <div className="col-lg-8 offset-lg-3 col-md-11 offset-md-2">
             <div className={styles["form"]}>
               <div className={styles["title-form"]}>
-                <h1>Your email is wrong!</h1>
+                <h1>Forgot your Password?</h1>
                 <p>
                   Don't worry! Just fill in your email and we'll send you a link
                 </p>
@@ -31,13 +52,15 @@ function ForgotPassword() {
                 <form action="">
                   <input
                     type="text"
+                    name="email"
                     placeholder="User name or email address *"
+                    onChange={changeHandler}
                   />
-                  <p>Forget your password?</p>
+                  {/* <p>Forget your password?</p> */}
                 </form>
               </div>
               <div className={styles["btn-login"]}>
-                <button>Reset Password</button>
+                <button onClick={submitHandler}>Get Code</button>
               </div>
             </div>
           </div>
