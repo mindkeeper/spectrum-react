61cf675b0a263920343d46adb75089b26654f062
rofa <zainrofa29@gmail.com>
2022-11-18 17:15:45 +0700

    ada code otp page

---
+++ src/pages/forgotpassword/OTP.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+import styles from "./OTP.module.css";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import authActions from "../../redux/actions/auths";
+
+function OTP() {
+  const navigate = useNavigate();
+  const dispacth = useDispatch();
+  const [body, setBody] = useState({});
+  const email = JSON.parse(localStorage.getItem("email"));
+
+  const changeHandler = (e) =>
+    setBody({
+      ...body,
+      [e.target.name]: e.target.value,
+      email: email,
+    });
+  console.log(body);
+
+  const toLogin = () => navigate("/login");
+
+  const submitHandler = () => {
+    dispacth(authActions.resetThunk(body, toLogin));
+    localStorage.removeItem("email");
+  };
+  return (
+    <>
+      <Header />
+      <div className="container-fluid">
+        <div className="row">
+          <main className={`col-lg-12 ${styles["page-title"]}`}>
+            <h1>My Account</h1>
+            <p>
+              Register and log in with your account to be able to shop at will
+            </p>
+          </main>
+        </div>
+      </div>
+      <div className="container-fluid">
+        <div className={`row ${styles["form-container"]}`}>
+          <div className="col-lg-8 offset-lg-3 col-md-11 offset-md-2">
+            <div className={styles["form"]}>
+              <div className={styles["title-form"]}>
+                <h1>Forgot your Password?</h1>
+                <p>
+                  Don't worry! Just fill in your email and we'll send you a link
+                </p>
+              </div>
+              <div className={styles["form-login"]}>
+                <form action="">
+                  <input
+                    type="text"
+                    name="code"
+                    placeholder="Input Code OTP *"
+                    onChange={changeHandler}
+                  />
+                  <input
+                    type="password"
+                    name="new_password"
+                    placeholder="Input Your New Password *"
+                    onChange={changeHandler}
+                  />
+                  {/* <p>Forget your password?</p> */}
+                </form>
+              </div>
+              <div className={styles["btn-login"]}>
+                <button onClick={submitHandler}>Reset Password</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default OTP;
