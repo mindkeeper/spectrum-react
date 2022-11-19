b57c4451522102533d6711f7a90ba7c8e6bcc769
rofa <zainrofa29@gmail.com>
2022-11-19 15:04:18 +0700

    add product reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "./auths";
+import productReducer from "./product";
 import registerReducer from "./register";
 
 export default combineReducers({
   auth: authReducer,
   register: registerReducer,
+  products: productReducer,
 });
