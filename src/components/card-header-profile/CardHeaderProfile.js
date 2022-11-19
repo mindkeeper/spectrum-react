1695eafa5359262d9895beb16c5c164d3148b30a
rofa <zainrofa29@gmail.com>
2022-11-19 19:45:31 +0700

    fixing

--- src/components/card-header-profile/CardHeaderProfile.js
+++ src/components/card-header-profile/CardHeaderProfile.js
@@ -1,33 +1,43 @@
-import React from 'react'
-
+import React from "react";
 
 // import CSS
-import css from "./CardHeaderProfile.module.css"
+import css from "./CardHeaderProfile.module.css";
 
 function CardHeaderProfile() {
-    return (
-        <>
-            <div className="container-fluid">
-                <div className={`container py-5 ${css['box']}`}>
-                    <div className="row d-flex flex-row align-items-center justify-content-start">
-                        <div className={`col-lg-2 col-md-3 col-sm-12 d-flex justify-content-center ${css["header-profile-one"]}`}>
-                            <span>Profile</span>
-                        </div>
-                        <div className={`col-lg-2 col-md-3 col-sm-12 d-flex justify-content-center ${css["header-profile-two"]}`}>
-                            <span>My Product<i class="fa-solid fa-chevron-down fs-6 ms-3"></i></span>
-                        </div>
-                        <div className={`col-lg-2 col-md-3 col-sm-12 d-flex justify-content-center ${css["header-profile-three"]}`}>
-                            <span>Selling Product</span>
-                        </div>
-                        <div className={`col-lg-2 col-md-3 col-sm-12 d-flex justify-content-center ${css["header-profile-four"]}`}>
-                            <span>My Order<i class="fa-solid fa-chevron-down fs-6 ms-3"></i></span>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="container-fluid">
+        <div className={`container py-5 ${css["box"]}`}>
+          <div className="row d-flex flex-row align-items-center justify-content-start">
+            <div
+              className={`col-lg-2 col-md-3 col-sm-12 d-flex justify-content-center ${css["header-profile-one"]}`}
+            >
+              <span>Profile</span>
             </div>
-        </>
-    )
+            <div
+              className={`col-lg-2 col-md-3 col-sm-12 d-flex justify-content-center ${css["header-profile-two"]}`}
+            >
+              <span>
+                My Product<i className="fa-solid fa-chevron-down fs-6 ms-3"></i>
+              </span>
+            </div>
+            <div
+              className={`col-lg-2 col-md-3 col-sm-12 d-flex justify-content-center ${css["header-profile-three"]}`}
+            >
+              <span>Selling Product</span>
+            </div>
+            <div
+              className={`col-lg-2 col-md-3 col-sm-12 d-flex justify-content-center ${css["header-profile-four"]}`}
+            >
+              <span>
+                My Order<i className="fa-solid fa-chevron-down fs-6 ms-3"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-
-export default CardHeaderProfile
+export default CardHeaderProfile;
