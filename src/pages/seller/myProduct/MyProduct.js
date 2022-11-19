3e32508db5f96bcad7c0a4f6caefc72f64369282
rofa <zainrofa29@gmail.com>
2022-11-19 21:28:44 +0700

    add my product seller page

--- src/pages/seller/myProduct/MyProduct.js
+++ src/pages/seller/myProduct/MyProduct.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./MyProduct.module.css";
 import Header from "../../../components/header/Header";
+import Tab from "../../../components/card-header-profile/CardHeaderProfile";
 import Footer from "../../../components/footer/Footer";
+import Card from "../../../components/cardMyProduct/CardMyProduct";
 
 function MyProduct() {
+  const [dropdown, setDropdown] = useState(false);
+  const active = () => {
+    setDropdown(!dropdown);
+  };
   return (
     <>
       <Header />
@@ -15,6 +21,35 @@ function MyProduct() {
               Register and log in with your account to be able to shop at will
             </p>
           </main>
+        </div>
+      </div>
+      <Tab show={active} />
+      {dropdown && (
+        <div className={styles["dropdown"]}>
+          <ol>
+            <li>All</li>
+            <li>Archieve</li>
+            <li>Sold Out</li>
+          </ol>
+        </div>
+      )}
+
+      <div className="container">
+        <div className={`row ${styles["product-tab"]}`}>
+          <div className="col-4">
+            <h1>Product</h1>
+          </div>
+          <div className="col-3">
+            <h1>Stock Status</h1>
+          </div>
+          <div className="col-2">
+            <h1>Price</h1>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className={`row ${styles["product-content"]}`}>
+          <Card />
         </div>
       </div>
       <Footer />
