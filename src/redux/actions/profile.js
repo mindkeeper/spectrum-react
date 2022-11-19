262dcb6a77e6bfa615d28203b4d552af342cf1f2
farisan <muhammad.farisan99@gmail.com>
2022-11-19 19:03:54 +0700

    add redux profile  (get + patch)

---
+++ src/redux/actions/profile.js
@@ -0,0 +1,59 @@
+import { ActionType } from 'redux-promise-middleware'
+import { getProfile, patchProfile } from "../../utils/fetchers"
+import { ACTION_STRING } from './actionStrings'
+
+const { Pending, Rejected, Fulfilled } = ActionType;
+
+const dataProfilePending = () => ({
+    type: ACTION_STRING.dataProfile.concat("_", Pending)
+});
+const dataProfileRejected = (error) => ({
+    type: ACTION_STRING.dataProfile.concat("_", Rejected),
+    payload: { error },
+});
+const dataProfileFulfilled = (data) => ({
+    type: ACTION_STRING.dataProfile.concat("_", Fulfilled),
+    payload: { data },
+});
+
+const patchProfilePending = () => ({
+    type: ACTION_STRING.editProfile.concat("_", Pending)
+});
+const patchProfileRejected = (error) => ({
+    type: ACTION_STRING.editProfile.concat("_", Rejected),
+    payload: { error },
+});
+const patchProfileFulfilled = (data) => ({
+    type: ACTION_STRING.editProfile.concat("_", Fulfilled),
+    payload: { data },
+});
+
+const getProfileThunk = (token) => {
+    return async (dispacth) => {
+        try {
+            dispacth(dataProfilePending())
+            const result = await getProfile(token)
+            dispacth(dataProfileFulfilled(result.data))
+            console.log(result.data)
+        } catch (error) {
+            dispacth(dataProfileRejected(error));
+        }
+    }
+}
+
+const editProfileThunk = (body, token) => {
+    return async (dispacth) => {
+        try {
+            dispacth(patchProfilePending())
+            const result = await patchProfile(body, token)
+            dispacth(patchProfileFulfilled(result.data))
+            console.log(result.data)
+        } catch (error) {
+            dispacth(patchProfileRejected(error));
+        }
+    }
+}
+
+const profileActions = { getProfileThunk, editProfileThunk }
+
+export default profileActions
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "./auths";
 import registerReducer from "./register";
+import profileReducer from './profile'
 
 export default combineReducers({
   auth: authReducer,
   register: registerReducer,
+  data_profile: profileReducer,
 });
