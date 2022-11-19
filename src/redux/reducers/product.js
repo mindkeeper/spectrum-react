dff669bd1519ae923e70e19f2a7a9fc0589b1d73
rofa <zainrofa29@gmail.com>
2022-11-19 15:03:51 +0700

    add redux thunk reducer

---
+++ src/redux/reducers/product.js
@@ -0,0 +1,43 @@
+import { ActionType } from "redux-promise-middleware";
+import { ACTION_STRING } from "../actions/actionStrings";
+
+const initialState = {
+  products: [],
+  isError: false,
+  isLoading: false,
+  isFulfilled: false,
+};
+
+const productReducer = (prevState = initialState, { type, payload }) => {
+  const { Pending, Rejected, Fulfilled } = ActionType;
+  const { getProducts } = ACTION_STRING;
+
+  switch (type) {
+    case getProducts.concat("_", Pending):
+      return {
+        ...prevState,
+        isLoading: true,
+        isError: false,
+        isFulfilled: false,
+      };
+    case getProducts.concat("_", Rejected):
+      return {
+        ...prevState,
+        isLoading: false,
+        isError: true,
+        isFulfilled: false,
+      };
+    case getProducts.concat("_", Fulfilled):
+      return {
+        ...prevState,
+        isLoading: false,
+        isError: false,
+        isFulfilled: true,
+        products: payload.data.data,
+      };
+    default:
+      return prevState;
+  }
+};
+
+export default productReducer;
