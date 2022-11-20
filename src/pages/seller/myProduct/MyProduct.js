9df3a0fc13761db7ae376766dab39927435173e3
farisan <muhammad.farisan99@gmail.com>
2022-11-20 10:58:35 +0700

    page myproduct

---
+++ src/pages/seller/myProduct/MyProduct.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import styles from "./MyProduct.module.css";
+import Header from "../../../components/header/Header";
+import Tab from "../../../components/card-header-profile/CardHeaderProfile";
+import Footer from "../../../components/footer/Footer";
+import Card from "../../../components/cardMyProduct/CardMyProduct";
+
+function MyProduct() {
+  const [dropdown, setDropdown] = useState(false);
+  const active = () => {
+    setDropdown(!dropdown);
+  };
+  return (
+    <>
+      <Header />
+      <div className="container-fluid">
+        <div className="row">
+          <main className={`col-lg-12 ${styles["page-title"]}`}>
+            <h1>My Account</h1>
+            <p>
+              Register and log in with your account to be able to shop at will
+            </p>
+          </main>
+        </div>
+      </div>
+      <Tab show={active} />
+      {dropdown && (
+        <div className={styles["dropdown"]}>
+          <ol>
+            <li>All</li>
+            <li>Archieve</li>
+            <li>Sold Out</li>
+          </ol>
+        </div>
+      )}
+
+      <div className="container">
+        <div className={`row ${styles["product-tab"]}`}>
+          <div className="col-4">
+            <h1>Product</h1>
+          </div>
+          <div className="col-3">
+            <h1>Stock Status</h1>
+          </div>
+          <div className="col-2">
+            <h1>Price</h1>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className={`row ${styles["product-content"]}`}>
+          <Card />
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default MyProduct;
