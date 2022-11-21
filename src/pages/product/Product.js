47fc5eb6b6452db7c13570e260aaa67fc40c8cd8
rofa <zainrofa29@gmail.com>
2022-11-21 19:51:37 +0700

    fixing

--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import title from "../../components/title/Title";
