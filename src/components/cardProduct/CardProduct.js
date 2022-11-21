9dee4f86c4cf74a585116219b3aa53432699327e
rofa <zainrofa29@gmail.com>
2022-11-21 15:34:25 +0700

    add navigate to product detail with param id

--- src/components/cardProduct/CardProduct.js
+++ src/components/cardProduct/CardProduct.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./CardProduct.module.css";
 
 // import sample from "../../asset/product/yellow-chair.png";
 
 function CardProduct(props) {
+  const navigate = useNavigate();
+
+  // const toDetail = navigate(`/product/detail/${props.id}`);
+
   return (
     <div className={styles["card-product"]}>
       <div className={styles["image"]}>
-        <img src={props.image} alt="" />
+        <img
+          src={props.image}
+          alt=""
+          onClick={() => navigate(`/product/detail/${props.id}`)}
+        />
       </div>
       <div className={styles["name"]}>
         <h1>{props.productName}</h1>
