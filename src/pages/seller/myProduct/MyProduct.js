c3e195eb22132dceed6c9be27475675ff97d5bb9
rofa <zainrofa29@gmail.com>
2022-11-22 00:56:04 +0700

    add get, filter product

--- src/pages/seller/myProduct/MyProduct.js
+++ src/pages/seller/myProduct/MyProduct.js
@@ -1,16 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./MyProduct.module.css";
 import Header from "../../../components/header/Header";
 import Tab from "../../../components/card-header-profile/CardHeaderProfile";
 import Footer from "../../../components/footer/Footer";
 import Card from "../../../components/cardMyProduct/CardMyProduct";
 import title from "../../../components/title/Title";
+import { useDispatch, useSelector } from "react-redux";
+import productActions from "../../../redux/actions/product";
+// import authActions from "../../../redux/actions/auths";
+import { createSearchParams, useSearchParams } from "react-router-dom";
+import Loading from "../../../components/loading/Loading";
 
 function MyProduct() {
   const [dropdown, setDropdown] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [query, setQuery] = useState({});
+  const dispatch = useDispatch();
+  const product = useSelector((state) => state.products.productSeller);
+  const isLoading = useSelector((state) => state.products.isLoading);
+  const token = useSelector((state) => state.auth.userInfo.token);
+  console.log(product);
+  console.log(token);
   const active = () => {
     setDropdown(!dropdown);
   };
+
+  useEffect(() => {
+    const urlSearchParams = createSearchParams({ ...query });
+    setSearchParams(urlSearchParams);
+    dispatch(productActions.getProductSellerThunk(token, query));
+  }, [query, searchParams]);
 
   title("Spectrum | My Product");
   return (
@@ -30,29 +49,66 @@ function MyProduct() {
       {dropdown && (
         <div className={styles["dropdown"]}>
           <ol>
-            <li>All</li>
-            <li>Archieve</li>
-            <li>Sold Out</li>
+            <li
+              onClick={() => {
+                setQuery({});
+              }}
+            >
+              All
+            </li>
+            <li
+              onClick={() => {
+                setQuery({
+                  ...query,
+                  filter: "archived",
+                });
+              }}
+            >
+              Archieve
+            </li>
+            <li
+              onClick={() => {
+                setQuery({
+                  ...query,
+                  filter: "sold-out",
+                });
+              }}
+            >
+              Sold Out
+            </li>
           </ol>
         </div>
       )}
 
       <div className="container">
         <div className={`row ${styles["product-tab"]}`}>
-          <div className="col-4">
+          <div className="col-lg-4 col-md-4 col-4">
             <h1>Product</h1>
           </div>
-          <div className="col-3">
+          <div className="col-lg-3 col-md-3 col-2">
             <h1>Stock Status</h1>
           </div>
-          <div className="col-2">
+          <div className="col-lg-2 col-md-2 col-2">
             <h1>Price</h1>
           </div>
         </div>
       </div>
       <div className="container">
         <div className={`row ${styles["product-content"]}`}>
-          <Card />
+          {isLoading ? (
+            <Loading />
+          ) : (
+            product?.map((e) => (
+              <Card
+                image={e.image}
+                name={e.product_name}
+                stock={e.stock}
+                price={e.price}
+                id={e.id}
+                key={e.id}
+              />
+            ))
+          )}
         </div>
       </div>
       <Footer />
